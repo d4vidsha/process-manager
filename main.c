175a9d583477540b51d6e1a105e02e972eb8730b
David Sha <dasha@student.unimelb.edu.au>
2023-03-30 16:05:30 +1100

    fix: create template of process manager; pcb error

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include <sys/types.h>
 #include "main.h"
 #include "linkedlist.h"
+#include "config.h"
 
 const char *const SCHEDULERS[] = {"SJF", "RR", NULL};
 const char *const MEMORY_METHODS[] = {"infinite", "best-fit", NULL};
@@ -22,12 +23,12 @@ int main(int argc, char *argv[]) {
     assert(args.quantum != NULL);
 
     // run simulation
-    simulation(args);
+    process_manager(args);
 
     return 0;
 }
 
-void simulation(args_t args) {
+void process_manager(args_t args) {
     /*  Given a struct of arguments that contains the file, scheduler,
         memory method, and quantum, run the simulation.
     */
@@ -47,26 +48,24 @@ void simulation(args_t args) {
     assert(fp);
 
     // for each line in the file, parse it and add it to a linked list
-    list_t *pcbs = create_empty_list();
+    list_t *submitted_pcbs = create_empty_list();
     char *line = NULL;
     size_t len = 0;
     ssize_t read;
     while ((read = getline(&line, &len, fp)) != -1) {
-        pcb_t pcb = parse_pcb_line(line);
-
-        if (DEBUG) {
-            printf("name: %s, arrive: %" PRIu32 "s, service: %" PRIu32
-                   "s, mem: %" PRIu16 "MB\n",
-                   pcb.name, pcb.arrival_time, pcb.service_time,
-                   pcb.memory_size);
-        }
-
-        // add to linked list
-        pcbs = append(pcbs, &pcb);
+        pcb_t *pcb = parse_pcb_line(line);
+        append(submitted_pcbs, pcb);
     }
 
-    // int simulation_time = 0;
-    // int cycles = 0;
+    node_t *curr = submitted_pcbs->head;
+    while (curr) {
+        pcb_t *pcb = (pcb_t *)curr->data;
+        printf("name: %s, arrive: %" PRIu32 "s, service: %" PRIu32
+               "s, mem: %" PRIu16 "MB, state: %d\n",
+               pcb->name, pcb->arrival_time, pcb->service_time,
+               pcb->memory_size, pcb->state);
+        curr = curr->next;
+    }
 
     // free line if necessary
     if (line) {
@@ -77,13 +76,112 @@ void simulation(args_t args) {
     fclose(fp);
 
     if (DEBUG) {
-        printf("Number of processes in linked list: %d\n", list_len(pcbs));
+        printf("Number of processes in linked list: %d\n",
+               list_len(submitted_pcbs));
     }
+
+    list_t *input_queue = create_empty_list();
+    list_t *ready_queue = create_empty_list();
+    int quantum = atoi(args.quantum);
+    int simulation_time = 0;
+    while (is_empty_list(submitted_pcbs) == FALSE) {
+        printf("Simulation time: %d\n", simulation_time);
+
+        // update the state of each process in the system
+        node_t *curr;
+        curr = submitted_pcbs->head;
+        while (curr) {
+            pcb_t *pcb = (pcb_t *)curr->data;
+            if (pcb->arrival_time <= simulation_time) {
+                pcb->state = READY;
+            }
+            if (pcb->state == RUNNING) {
+                pcb->service_time -= quantum;
+                if (pcb->service_time <= 0) {
+                    pcb->state = FINISHED;
+                }
+            }
+            curr = curr->next;
+        }
+
+        // move processes to input queue if they have READY state
+        curr = submitted_pcbs->head;
+        node_t *next = NULL;
+        while (curr) {
+            pcb_t *pcb = (pcb_t *)curr->data;
+            next = curr->next;
+            if (pcb->state == READY) {
+                move_data(pcb, submitted_pcbs, input_queue);
+            }
+            curr = next;
+        }
+
+        // print submitted queue
+        printf("Submitted queue: ");
+        curr = submitted_pcbs->head;
+        while (curr) {
+            pcb_t *pcb = (pcb_t *)curr->data;
+            // assert(pcb->name != NULL);
+            // assert(pcb->state == READY);
+            printf("%s ", pcb->name);
+            curr = curr->next;
+        }
+        printf("\n");
+
+        // print input queue
+        printf("Input queue: ");
+        curr = input_queue->head;
+        while (curr) {
+            pcb_t *pcb = (pcb_t *)curr->data;
+            assert(pcb->name != NULL);
+            assert(pcb->state == READY);
+            printf("%s ", pcb->name);
+            curr = curr->next;
+        }
+        printf("\n");
+
+        // // run one cycle
+        // cycle_tasks();
+
+        simulation_time += quantum;
+
+        // if all processes have been completed, terminate the simulation
+        // if (all_processes_completed()) {
+        //     break;
+        // }
+    }
+
+    // free linked lists
+    free_list(submitted_pcbs, free_pcb);
+    free_list(input_queue, free_pcb);
+    free_list(ready_queue, free_pcb);
+    
 }
 
-void cycle_tasks() {
+// what are the inputs and outputs of this function?
+void cycle_tasks(pcb_t *running, list_t *input, list_t *ready) {
     /*  Run one cycle.
      */
+
+    // if current running process (if any) has completed, terminate it and
+    // deallocate its memory
+
+    // identify all processes that have been submitted since the last cycle
+    // occurred and add them to the input queue in the order they appear
+    // in the process file. A process is considered to have been submitted to
+    // the system if its arrival time is less than or equal to the current
+    // simulation time
+
+    // move processes from the input queue to the ready queue upon successful
+    // memory allocation. The ready queue holds all processes that are ready
+    // to run
+
+    // memory allocation can be done in one of two ways: infinite or best-fit
+
+    // determine the process (if any) that will run in this cycle. Depending
+    // on the scheduling algorithm, this may be the process that has been
+    // previously running, or a ready process which has not been previously
+    // running
 }
 
 char *read_flag(char *flag, const char *const *valid_args, int argc,
@@ -136,23 +234,33 @@ args_t parse_args(int argc, char *argv[]) {
     return args;
 }
 
-pcb_t parse_pcb_line(char *line) {
+pcb_t *parse_pcb_line(char *line) {
     /*  Given a line from the input file, parse it and return a pcb_t
         struct.
 
         The line should be in the format:
         <arrival time> <name> <service time> <memory size>
     */
-    pcb_t pcb;
+    pcb_t *pcb;
+    pcb = (pcb_t *)malloc(sizeof(*pcb));
     char *token = strtok(line, SEPARATOR);
-    pcb.arrival_time = (uint32_t)strtoul(token, NULL, 10);
+    pcb->arrival_time = (uint32_t)strtoul(token, NULL, 10);
     token = strtok(NULL, SEPARATOR);
-    pcb.name = token;
+    pcb->name = memcpy(malloc(strlen(token) + 1), token, strlen(token) + 1);
     token = strtok(NULL, SEPARATOR);
-    pcb.service_time = (uint32_t)strtoul(token, NULL, 10);
+    pcb->service_time = (uint32_t)strtoul(token, NULL, 10);
     token = strtok(NULL, SEPARATOR);
-    pcb.memory_size = (uint16_t)strtoul(token, NULL, 10);
+    pcb->memory_size = (uint16_t)strtoul(token, NULL, 10);
+    pcb->state = NEW;
     return pcb;
+}
+
+void free_pcb(void *data) {
+    /*  Free a pcb_t struct.
+    */
+    pcb_t *pcb = (pcb_t *)data;
+    free(pcb->name);
+    free(pcb);
 }
 
 /* =============================================================================
--- main.h
+++ main.h
@@ -21,14 +21,17 @@ typedef struct process_control_block {
     uint32_t arrival_time;
     uint32_t service_time;
     uint16_t memory_size;
+    enum state {NEW, READY, RUNNING, FINISHED} state;
 } pcb_t;
 
 /* function prototypes ====================================================== */
 char *read_flag(char *flag, const char *const *valid_args, int argc,
                 char *argv[]);
 args_t parse_args(int argc, char *argv[]);
-void simulation(args_t args);
-pcb_t parse_pcb_line(char *line);
+void process_manager(args_t args);
+pcb_t *parse_pcb_line(char *line);
+void cycle_tasks();
+void free_pcb(void *data);
 
 #endif
 /* =============================================================================
