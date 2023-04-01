ea8c5521d05df8b5f074f5c06d9ffccee0778dc1
David Sha <dasha@student.unimelb.edu.au>
2023-04-01 19:13:35 +1100

    refactor: make more readable; add MAX_MEMORY

--- config.h
+++ config.h
@@ -6,9 +6,19 @@
 #define TRUE 1
 #define FALSE 0
 
-// main.c
+/*  Separator for parsing input files.
+*/
 #define SEPARATOR " "
+
+/*  Configure debug mode. Lots of useful information will be printed to stdout.
+*/
 #define DEBUG 0
+
+/*  In MB. The maximum amount of memory in the simulated system. The allowed
+    max is 2^16 = 65536 because we use uint16_t for pcb_t.memory_size,
+    block.location, and block.size.
+*/
+#define MAX_MEMORY 2048
 
 #endif
 /* =============================================================================
